d813fc61440f235ed191d278c89b0a5db8268e04
Sagar Johari <joharisagar53@gmail.com>
2025-07-26 23:47:48 +0530

    changes on portfolio

---
+++ src/app/components/header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+const header = () => {
+    return (
+        <header>
+            <h1>Header</h1>
+        </header>
+    )
+}
+
+export default header;
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Header from "./components/header";
+export default function Home() {
+  return (
+    <div className="font-sans grid  items-center justify-items-center min-h-screen">
+      <Header/>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+       
+      </footer>
+    </div>
+  );
+}
